5b0692b5ceb4a7c9253c992a7d128b0fd619353d
Esmaeeil enani <esmaeeilenani@gmail.com>
2023-06-23 17:41:28 +0300

    #36 (#37)
    setup test container and test profile with database migration
    enable test in ci

---
+++ carRental/src/test/java/com/amigoscode/chohort2/carRental/AbstractTestContainer.java
@@ -0,0 +1,43 @@
+package com.amigoscode.chohort2.carRental;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.TestPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import javax.sql.DataSource;
+
+@Testcontainers
+//@TestPropertySource(properties = "spring.datasource.url=jdbc:tc:postgresql:latest:///")
+public abstract class AbstractTestContainer {
+
+    @Container
+    protected static final PostgreSQLContainer<?> postgreSQLContainer =
+            new PostgreSQLContainer<>("postgres:latest")
+                    .withDatabaseName("unit-test")
+                    .withUsername("root")
+                    .withPassword("123");
+
+    @DynamicPropertySource
+    private static void registerDataSourceProperties(DynamicPropertyRegistry registry){
+
+        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+    }
+
+    protected static DataSource getDataSource(){
+        return DataSourceBuilder
+                .create()
+                .driverClassName(postgreSQLContainer.getDriverClassName())
+                .url(postgreSQLContainer.getJdbcUrl())
+                .username(postgreSQLContainer.getUsername())
+                .password(postgreSQLContainer.getPassword())
+                .build();
+    }
+
+
+}
