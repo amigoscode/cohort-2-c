aa4231ce854352bf65d3c2bd971e7dfcd67cef41
Esmaeeil enani <esmaeeilenani@gmail.com>
2023-06-30 16:53:09 +0300

    Es/feat/#63 (#68)
    * #63
    register and login for client user
    
    * #63
    generalize some specification function
    
    * #63
    generalize some specification function
    
    * #63
    add unit and IT testing for Registration
    
    * #63
    fix IT testing for Registration
    
    * #63
    add surefire config -Dspring.profiles.active=test
    
    * #63
    add surefire config -Dspring.profiles.active=test
    
    * #63
    add mvn install to run IT test
    
    * #63
    
    * #63

--- carRental/src/main/java/com/amigoscode/chohort2/carRental/driverLicense/VM/DriverLicenseVM.java
+++ carRental/src/main/java/com/amigoscode/chohort2/carRental/driverLicense/VM/DriverLicenseVM.java
@@ -8,12 +8,14 @@ import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import lombok.experimental.Accessors;
 
 import java.time.LocalDate;
 
 @Setter
 @Getter
 @ToString
+@Accessors(chain = true)
 public class DriverLicenseVM {
 
     @NotEmpty
