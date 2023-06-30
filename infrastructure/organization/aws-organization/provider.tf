72c5dd48a7ecde15adc480d1764c8e428d93bd14
Dmitri Konnov <44975416+DmitriKonnovNN@users.noreply.github.com>
2023-06-30 18:24:52 +0400

    Es/feat/#63 (#67) (#69)
    * #63
    register and login for client user
    
    * #63
    generalize some specification function
    
    * #63
    generalize some specification function
    
    Co-authored-by: Esmaeeil enani <esmaeeilenani@gmail.com>

--- infrastructure/organization/aws-organization/provider.tf
+++ infrastructure/organization/aws-organization/provider.tf
@@ -1,4 +1,15 @@
 provider "aws" {
   profile = var.master_aws_profile
   region  = var.master_account_default_region
+  #####################################################################################################################
+  # Default Tag(s) which are attached to each resource capable of having a tag. Default tag(s) can be overridden by a
+  #  in-place tag definition;
+  #####################################################################################################################
+  default_tags {
+    tags = {
+      Environment  = var.default_tag_environment
+      CreatedVia   = var.default_tag_created_by
+      Organization = var.default_org_tag
+    }
+  }
 }
