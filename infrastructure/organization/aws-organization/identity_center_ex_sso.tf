fa528f45def099ffc1dbccd31995a2cfbd379ba9
Dmitri Konnov <lildkonnov.nnov@inbox.ru>
2023-07-02 20:14:18 +0400

    sso added

---
+++ infrastructure/organization/aws-organization/identity_center_ex_sso.tf
@@ -0,0 +1,75 @@
+data "aws_ssoadmin_instances" "sso" {}
+data "aws_identitystore_group" "id_group" {
+  for_each          = toset(local.groups)
+  identity_store_id = local.identity_store_id
+
+  alternate_identifier {
+    unique_attribute {
+      attribute_path  = "DisplayName"
+      attribute_value = each.key
+    }
+  }
+}
+
+resource "aws_ssoadmin_permission_set" "permission_sets" {
+  for_each = {
+    for record in var.sso_permissions : record.name => record
+  }
+  #  for_each = var.sso_permissions
+  instance_arn     = tolist(data.aws_ssoadmin_instances.sso.arns)[0]
+  name             = each.value.name
+  description      = each.value.description
+  session_duration = each.value.session_duration
+}
+resource "aws_ssoadmin_account_assignment" "account_assignment" {
+  for_each           = local.account_group_assignments
+  instance_arn       = local.sso_instance_arn
+  permission_set_arn = aws_ssoadmin_permission_set.permission_sets[each.value.permission_set_name].arn
+  principal_id       = data.aws_identitystore_group.id_group[each.value.group].id
+  target_id          = each.value.account
+  principal_type     = "GROUP"
+  target_type        = "AWS_ACCOUNT"
+}
+
+resource "aws_ssoadmin_customer_managed_policy_attachment" "managed_policy_attachment" {
+  for_each           = local.managed_policy_arns
+  instance_arn       = local.sso_instance_arn
+  managed_policy_arn = each.value.policy_arn
+  permission_set_arn = aws_ssoadmin_permission_set.permission_sets[each.value.permission_set_name].arn
+}
+
+locals {
+  managed_policies = flatten([
+    for permission in var.sso_permissions : [
+      for policy in permission.managed_policies : {
+        permission_set_name = permission.name
+        policy_arn          = policy
+      }
+    ]
+  ])
+
+  managed_policy_arns = {
+    for policy in local.managed_policies :
+    "${policy.permission_set_name}.${policy.policy_arn}" => policy
+  }
+
+  sso_instance_arn  = tolist(data.aws_ssoadmin_instances.sso.arns)[0]
+  identity_store_id = tolist(data.aws_ssoadmin_instances.sso.identity_store_ids)[0]
+
+  account_groups = flatten([
+    for permission in var.sso_permissions : [
+      for account_group in setproduct(permission.aws_accounts, permission.sso_groups) : {
+        permission_set_name = permission.name
+        account             = account_group[0]
+        group               = account_group[1]
+      }
+    ]
+  ])
+
+  account_group_assignments = {
+    for account_group in local.account_groups :
+    "${account_group.permission_set_name}.${account_group.account}.${account_group.group}" => account_group
+  }
+
+  groups = distinct([for account_group in local.account_groups : account_group.group])
+}
--- infrastructure/organization/aws-organization/variables.tf
+++ infrastructure/organization/aws-organization/variables.tf
@@ -66,7 +66,8 @@ variable "aws_service_access_principals" {
   type = list(string)
   default = [
     "cloudtrail.amazonaws.com",
-    "config.amazonaws.com"
+    "config.amazonaws.com",
+    "sso.amazonaws.com"
   ]
   nullable    = false
   description = "A list of AWS service principal names for which you want to enable integration with your organization."
@@ -122,4 +123,64 @@ variable "accounts" {
     }
   }
   description = "aws accounts deployed from master account"
+}
+
+variable "sso_permissions" {
+  type = list(object({
+    name             = string
+    description      = string
+    session_duration = string
+    managed_policies = list(string)
+    aws_accounts     = list(string)
+    sso_groups       = list(string)
+  }))
+
+  default = [
+    {
+      name             = "AdministratorAccess"
+      description      = "Provides full access to AWS services and resources."
+      session_duration = "PT4H"
+      managed_policies = ["arn:aws:iam::aws:policy/AdministratorAccess"]
+      aws_accounts     = [""]
+      sso_groups       = ["AdministratorGroup"]
+
+    },
+    {
+      name             = "DatabaseAdministratorAccess"
+      description      = "Grants full access permissions to AWS services and actions required to set up and configure AWS database services."
+      session_duration = "PT4H"
+      managed_policies = ["arn:aws:iam::aws:policy/job-function/DatabaseAdministrator"]
+      aws_accounts     = [""]
+      sso_groups       = ["DatabaseAdministratorGroup"]
+    },
+    {
+      name             = "PowerUserAccess"
+      description      = "Provides full access to AWS services and resources, but does not allow management of Users and groups."
+      session_duration = "PT4H"
+      managed_policies = ["arn:aws:iam::aws:policy/PowerUserAccess"]
+      aws_accounts     = [""]
+      sso_groups       = ["PowerUserGroup"]
+    },
+    {
+      name             = "Billing"
+      description      = "Grants permissions for billing and cost management. This includes viewing account usage and viewing and modifying budgets and payment methods."
+      session_duration = "PT4H"
+      managed_policies = ["arn:aws:iam::aws:policy/job-function/Billing"]
+      aws_accounts     = [""]
+      sso_groups       = ["BillingGroup"]
+    },
+    {
+      name             = "ViewOnlyAccess"
+      description      = "This policy grants permissions to view resources and basic metadata across all AWS services."
+      session_duration = "PT4H"
+      managed_policies = ["arn:aws:iam::aws:policy/job-function/ViewOnlyAccess"]
+      aws_accounts     = [""]
+      sso_groups       = ["ViewOnlyGroup"]
+
+    }
+
+  ]
+  description = "list of permission set properties for Identity Center (successor to SSO)"
+  nullable    = false
+
 }
