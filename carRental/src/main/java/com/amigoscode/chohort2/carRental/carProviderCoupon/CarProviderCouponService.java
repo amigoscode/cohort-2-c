eb1b45fc535b3f19b5cf4a6d9a6bb083f2eb5cbf
Thays Vieira <thaysvieira39@gmail.com>
2023-07-05 15:32:26 +0200

    #76 serialization Vm class, exception for null values

--- carRental/src/main/java/com/amigoscode/chohort2/carRental/carProviderCoupon/CarProviderCouponService.java
+++ carRental/src/main/java/com/amigoscode/chohort2/carRental/carProviderCoupon/CarProviderCouponService.java
@@ -2,12 +2,11 @@ package com.amigoscode.chohort2.carRental.carProviderCoupon;
 
 import com.amigoscode.chohort2.carRental.annotation.TransactionalService;
 import com.amigoscode.chohort2.carRental.carProviderCoupon.VM.CarProviderCouponVM;
-import com.amigoscode.chohort2.carRental.carProviderUser.CarProviderUser;
 import com.amigoscode.chohort2.carRental.carProviderUser.CarProviderUserRepository;
+import com.amigoscode.chohort2.carRental.constants.ErrorConstants;
 import com.amigoscode.chohort2.carRental.user.User;
 import com.amigoscode.chohort2.carRental.user.UserService;
 import lombok.RequiredArgsConstructor;
-import java.util.Optional;
 
 @TransactionalService
 @RequiredArgsConstructor
@@ -28,7 +27,7 @@ public class CarProviderCouponService {
     }
     private Long getCarProvideId() {
         User user = userService.getLoggedInUser();
-        Optional<CarProviderUser> carProviderUserOptional = carProviderUserRepository.findByUserId(user.getId());
-        return carProviderUserOptional.get().getCarProviderId();
+        var carProviderUserOptional = carProviderUserRepository.findByUserId(user.getId()).orElseThrow(()-> new NullPointerException(ErrorConstants.INTERNAL));
+        return carProviderUserOptional.getCarProviderId();
     }
     }
--- carRental/src/main/java/com/amigoscode/chohort2/carRental/carProviderUser/CarProviderUserRepository.java
+++ carRental/src/main/java/com/amigoscode/chohort2/carRental/carProviderUser/CarProviderUserRepository.java
@@ -1,12 +1,11 @@
 package com.amigoscode.chohort2.carRental.carProviderUser;
-
-import com.amigoscode.chohort2.carRental.abstracts.repository.CRJpaRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface CarProviderUserRepository extends CRJpaRepository<CarProviderUser,Long> {
+public interface CarProviderUserRepository extends JpaRepository<CarProviderUser,Long> {
     Optional<CarProviderUser> findByUserId(@Param("user_id") Long userId);
 }
