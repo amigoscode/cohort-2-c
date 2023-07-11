272a91af4a9175a867f50100da2278197c0601eb
Thays Vieira <thaysvieira39@gmail.com>
2023-07-11 16:01:51 +0200

    #76 CRJ to JPA

--- carRental/src/main/java/com/amigoscode/chohort2/carRental/carProviderCoupon/CarProviderCouponRepository.java
+++ carRental/src/main/java/com/amigoscode/chohort2/carRental/carProviderCoupon/CarProviderCouponRepository.java
@@ -1,8 +1,8 @@
 package com.amigoscode.chohort2.carRental.carProviderCoupon;
 
-import com.amigoscode.chohort2.carRental.abstracts.repository.CRJpaRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CarProviderCouponRepository extends CRJpaRepository <CarProviderCoupon, Long> {
+public interface CarProviderCouponRepository extends JpaRepository<CarProviderCoupon, Long> {
 }
