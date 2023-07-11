68d24082e4740637fd2396cd357cbca2c9c362f7
Thays Vieira <thaysvieira39@gmail.com>
2023-07-11 16:37:48 +0200

    #76 CRJ to JPA

--- carRental/src/main/java/com/amigoscode/chohort2/carRental/carProviderCoupon/CarProviderCouponService.java
+++ carRental/src/main/java/com/amigoscode/chohort2/carRental/carProviderCoupon/CarProviderCouponService.java
@@ -26,12 +26,13 @@ public class CarProviderCouponService {
         var carProviderUserOptional = carProviderUserRepository.findByUserId(user.getId());
         if (carProviderUserOptional.get().getCarProviderId() == null)
             throw new ApiRequestException(ErrorConstants.USER_NOT_FOUND);
-        else return new CarProviderCoupon()
+        final var carProviderCoupon = new CarProviderCoupon()
                 .setCarProviderId(carProviderUserOptional.get().getCarProviderId())
                 .setCouponCode(carProviderCouponVM.getCouponCode())
                 .setStartDate(carProviderCouponVM.getStartDate())
                 .setEndDate(carProviderCouponVM.getEndDate())
                 .setNumOfUsePerUser(carProviderCouponVM.getNumOfUsePerUser())
                 .setIsAvailable(carProviderCouponVM.getIsAvailable());
+        return carProviderCoupon;
     }
     }
