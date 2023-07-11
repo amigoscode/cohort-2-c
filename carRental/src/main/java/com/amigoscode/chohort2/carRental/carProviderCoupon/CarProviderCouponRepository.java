6d99e2b0291ce36163c2c2680ab3f1d6801865d6
Thays Vieira <thaysvieira39@gmail.com>
2023-07-11 11:01:29 +0200

    #76 fixed crj for jpa and exception

--- carRental/src/main/java/com/amigoscode/chohort2/carRental/carProviderCoupon/CarProviderCouponRepository.java
+++ carRental/src/main/java/com/amigoscode/chohort2/carRental/carProviderCoupon/CarProviderCouponRepository.java
@@ -1,6 +1,8 @@
 package com.amigoscode.chohort2.carRental.carProviderCoupon;
 
 import com.amigoscode.chohort2.carRental.abstracts.repository.CRJpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CarProviderCouponRepository extends CRJpaRepository <CarProviderCoupon, Long> {
 }
--- carRental/src/main/java/com/amigoscode/chohort2/carRental/carProviderCoupon/CarProviderCouponService.java
+++ carRental/src/main/java/com/amigoscode/chohort2/carRental/carProviderCoupon/CarProviderCouponService.java
@@ -4,30 +4,34 @@ import com.amigoscode.chohort2.carRental.annotation.TransactionalService;
 import com.amigoscode.chohort2.carRental.carProviderCoupon.VM.CarProviderCouponVM;
 import com.amigoscode.chohort2.carRental.carProviderUser.CarProviderUserRepository;
 import com.amigoscode.chohort2.carRental.constants.ErrorConstants;
+import com.amigoscode.chohort2.carRental.exception.ApiRequestException;
 import com.amigoscode.chohort2.carRental.user.User;
 import com.amigoscode.chohort2.carRental.user.UserService;
 import lombok.RequiredArgsConstructor;
 
+
 @TransactionalService
 @RequiredArgsConstructor
 public class CarProviderCouponService {
-    private final CarProviderCouponRepository carProviderCouponRepository;
     private final CarProviderUserRepository carProviderUserRepository;
+    private  final CarProviderCouponRepository carProviderCouponRepository;
     private final UserService userService;
 
     public void save(CarProviderCouponVM carProviderCouponVM){
-        CarProviderCoupon carProviderCoupon = new CarProviderCoupon()
-                .setCarProviderId(getCarProvideId())
+        CarProviderCoupon carProviderCoupon=findCarProviderUser(carProviderCouponVM);
+        carProviderCouponRepository.save(carProviderCoupon);
+    }
+    private CarProviderCoupon findCarProviderUser(CarProviderCouponVM carProviderCouponVM) {
+        User user = userService.getLoggedInUser();
+        var carProviderUserOptional = carProviderUserRepository.findByUserId(user.getId());
+        if (carProviderUserOptional.get().getCarProviderId() != null)
+            throw new ApiRequestException(ErrorConstants.USER_NOT_FOUND);
+        else return new CarProviderCoupon()
+                .setCarProviderId(carProviderUserOptional.get().getCarProviderId())
                 .setCouponCode(carProviderCouponVM.getCouponCode())
                 .setStartDate(carProviderCouponVM.getStartDate())
                 .setEndDate(carProviderCouponVM.getEndDate())
                 .setNumOfUsePerUser(carProviderCouponVM.getNumOfUsePerUser())
                 .setIsAvailable(carProviderCouponVM.getIsAvailable());
-        carProviderCouponRepository.save(carProviderCoupon);
-    }
-    private Long getCarProvideId() {
-        User user = userService.getLoggedInUser();
-        var carProviderUserOptional = carProviderUserRepository.findByUserId(user.getId()).orElseThrow(()-> new NullPointerException(ErrorConstants.INTERNAL));
-        return carProviderUserOptional.getCarProviderId();
     }
     }
